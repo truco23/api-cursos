0daaec56e21d026a212ed52d39355bebc1f193c3
Truco Paulista <trucopaulista23@gmail.com>
2019-11-28 15:39:01 -0300

    Criando a estrutura de testes para a categoria

---
+++ tests/categories.test.js
@@ -0,0 +1,24 @@
+const { deepEqual } = require('assert')
+
+describe('Testes com as categorias', () => {
+
+    it('Testando conexão com o MongoDB', async () => {
+        deepEqual(true, true)
+    });
+
+    it('Testando o cadastro de categoria', async () => {
+        deepEqual(true, true)
+    });
+
+    it('Testando a listagem de categorias', async () => {
+        deepEqual(true, true)
+    });
+
+    it('Testando a atualização de categoria', async () => {
+        deepEqual(true, true)
+    });
+
+    it('Testando a remoção de categoria', async () => {
+        deepEqual(true, true)
+    })
+})

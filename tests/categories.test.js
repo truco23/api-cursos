7388f8a86d4174e53d06926b0cdbef3925e689c8
Truco Paulista <trucopaulista23@gmail.com>
2019-11-28 15:58:43 -0300

    Criando o teste para a conexão com o mongoDB

--- tests/categories.test.js
+++ tests/categories.test.js
@@ -1,9 +1,35 @@
 const { deepEqual } = require('assert')
+const mongoose = require('mongoose');
+let conected = false;
+
+function connection() {
+    mongoose.connect('mongodb://cursos:cursos@localhost:27017/cursos', { 
+        useNewUrlParser: true,
+        useCreateIndex :  true,
+        useUnifiedTopology: true
+    }, error => {
+        
+        if(!error) return error
+        console.log('Falha na conexão', error)
+    })
+}
 
 describe('Testes com as categorias', () => {
+    
+    before(async () => {
+
+        await connection()
+        await mongoose.connection.on('connected', function(){
+            conected = true
+        });
+    });
 
     it('Testando conexão com o MongoDB', async () => {
-        deepEqual(true, true)
+
+        const result = conected;
+        const expected = true;
+        
+        deepEqual(result, expected)
     });
 
     it('Testando o cadastro de categoria', async () => {

09b5f74afa111ac4bfa8c39920fab990b8b69ee5
Truco Paulista <trucopaulista23@gmail.com>
2019-11-29 17:44:08 -0300

    Criando o teste para cadastro e listagem de cursos

--- tests/courses.test.js
+++ tests/courses.test.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 require('../src/models/courses.model')
 const modelCourses = mongoose.model('schemaCourses')
 let conected = false;
-const addCourse = { name: 'Curso teste', description: 'Descrição teste' }
+const addCourse = { idCategory: '5de005219987c6351c7d4c0e', name: 'Curso teste', description: 'Descrição teste' }
 
 function connection() {
     mongoose.connect('mongodb://cursos:cursos@localhost:27017/cursos', { 
@@ -31,6 +31,26 @@ describe('Testes com os cursos', () => {
 
         const result = conected
         const expected = true
+
+        deepEqual(result, expected)
+    });
+
+    it('Testando o cadastro de curso', async () => {
+        
+        const [data] = await modelCourses.find({name: addCourse.name})
+        const course = {name: addCourse.name, description: addCourse.description}
+        const result = {name: data.name, description: data.description}
+        const expected = course
+        
+        deepEqual(result, expected)
+    })
+
+    it('Testando a listagem de cursos', async () => {
+
+        const [data] = await modelCourses.find({name: addCourse.name})
+        const course = {name: addCourse.name, description: addCourse.description}
+        const result = {name: data.name, description: data.description}
+        const expected = course
 
         deepEqual(result, expected)
     })

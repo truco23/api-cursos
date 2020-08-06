e001fceea750021f04aa9cef615e6f69bd518d07
G. Moraes Brasil <g.moraes.brasil@avanade.com>
2020-08-06 20:36:32 -0300

    Docker compose

---
+++ mongo-init.js
@@ -0,0 +1,14 @@
+db.createUser(
+    {
+        user: "cursos",
+        pwd: "cursos",
+        roles: [
+            {
+                role: "readWrite",
+                db: "cursos"
+            }
+        ]
+    }
+);
+
+db.auth('cursos', 'cursos')

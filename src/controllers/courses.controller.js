7658aa23f9872621e1c3a85f929135748b46b2e9
G. Moraes Brasil <g.moraes.brasil@avanade.com>
2020-08-24 21:30:38 -0300

    Correção alteração de cursos

--- src/controllers/courses.controller.js
+++ src/controllers/courses.controller.js
@@ -1,115 +1,118 @@
-const Mongoose = require('mongoose')
-const modelCourses = Mongoose.model('schemaCourses')
-const modelCategories = Mongoose.model('schemaCategories')
-const apiCourses = {}
-const log = require('../helpers/log.helpers')
+const Mongoose = require("mongoose");
+const modelCourses = Mongoose.model("schemaCourses");
+const modelCategories = Mongoose.model("schemaCategories");
+const apiCourses = {};
+const log = require("../helpers/log.helpers");
 
 apiCourses.get = async (req, res) => {
-    try {
-        const { page = 1, limit = 10 } = req.query
-        const options = {
-            page,
-            limit,
-            sort: {
-                createdAt: -1
-            },
-            populate: {
-                path: 'idCategory',
-                select: 'name'
-            }
-        }
-        const result = await modelCourses.paginate({}, options)
-        
-        log.list('Listagem de cursos', result)
+  try {
+    const { page = 1, limit = 10 } = req.query;
+    const options = {
+      page,
+      limit,
+      sort: {
+        createdAt: -1,
+      },
+      populate: {
+        path: "idCategory",
+        select: "name",
+      },
+    };
+    const result = await modelCourses.paginate({}, options);
 
-        return { result }
-    } catch (error) {
-        console.error(error);
-        return error.message
-    }
-}
+    log.list("Listagem de cursos", result);
+
+    return { result };
+  } catch (error) {
+    console.error(error);
+    return error.message;
+  }
+};
 
 apiCourses.getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await modelCourses
+      .find({ _id: id })
+      .populate({ path: "idCategory", select: "name" });
 
-    try {
-        const { id } = req.params
-        const result = await modelCourses.find({ _id: id }).populate({ path: 'idCategory', select: 'name'})
-        
-        log.list('Curso encontrado', result)
+    log.list("Curso encontrado", result);
 
-        return result
-    } catch (error) {
-        console.log(error);
-        return error.message
-    }
-}
+    return result;
+  } catch (error) {
+    console.log(error);
+    return error.message;
+  }
+};
 
 apiCourses.create = async (req, res) => {
+  try {
+    const { idCategory, name, description } = req.payload;
+    const result = await modelCourses.create({ idCategory, name, description });
 
-    try {
-        const { idCategory, name, description } = req.payload
-        const result = await modelCourses.create({idCategory, name, description})
-        
-        log.list('Novo curso criado', result)
+    log.list("Novo curso criado", result);
 
-        return { result }
-    } catch (error) {
-        console.error(error);
-        return error.message
-    }
-}
+    return { result };
+  } catch (error) {
+    console.error(error);
+    return error.message;
+  }
+};
 
 apiCourses.put = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.payload;
+    const values = JSON.stringify(body);
+    const valuesParse = JSON.parse(values);
 
-    try {
-        const { id } = req.params
-        const body = req.payload
-        const values = JSON.stringify(body)
-        const valuesParse = JSON.parse(values)
+    if (valuesParse.idCategory._id) {
+      const category = await modelCategories.find({
+        _id: valuesParse.idCategory._id,
+      });
 
-        if(valuesParse.idCategory) {
-            const category = await modelCategories.find({_id: valuesParse.idCategory})
-
-            if(!category) {
-                console.log('Categoria não encontrada')
-                return { error: 'Categoria não encontrada'}
-            }
-        }
-
-        const result = await modelCourses.findOneAndUpdate({ _id: id}, valuesParse)
-
-        result.set(body)
-        result.save()
-
-        log.list('Curso alterado', result)
-
-        return result
-    } catch (error) {
-        console.error(error);
-        return error.message
+      if (!category) {
+        console.log("Categoria não encontrada");
+        return { error: "Categoria não encontrada" };
+      }
     }
-}
+
+    const result = await modelCourses.findOneAndUpdate(
+      { _id: id },
+      valuesParse
+    );
+
+    result.set(body);
+    result.save();
+
+    log.list("Curso alterado", result);
+
+    return result;
+  } catch (error) {
+    console.error(error);
+    return error.message;
+  }
+};
 
 apiCourses.delete = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = await modelCourses.find({ _id: id });
 
-    try {
-        const { id } = req.params
-        const data = await modelCourses.find({ _id: id})
-        
-        if(!data.length) {
-            log.list('Curso não encontrado ou já foi removido')
-            return { warning: 'Curso não encontrado ou já foi removido'}
-        }
-        
-        const result = await modelCourses.deleteOne({ _id: id })
-
-        log.list('Curso removido', result)
-        
-        return { success: 'Curso removido' }
-    } catch (error) {
-        console.error(error);
-        return error.message
+    if (!data.length) {
+      log.list("Curso não encontrado ou já foi removido");
+      return { warning: "Curso não encontrado ou já foi removido" };
     }
-}
 
-module.exports = apiCourses
+    const result = await modelCourses.deleteOne({ _id: id });
+
+    log.list("Curso removido", result);
+
+    return { success: "Curso removido" };
+  } catch (error) {
+    console.error(error);
+    return error.message;
+  }
+};
+
+module.exports = apiCourses;
--- src/routes/courses.routes.js
+++ src/routes/courses.routes.js
@@ -1,113 +1,114 @@
-const coursesController = require('../controllers/courses.controller')
-const Joi = require('@hapi/joi')
+const coursesController = require("../controllers/courses.controller");
+const Joi = require("@hapi/joi");
 
 module.exports = server => {
+  const coursesRoutes = [
+    // listas todos os cursos
+    {
+      method: "GET",
+      path: "/courses",
+      options: {
+        description: "Rota de cursos",
+        notes: "Buscar todos os cursos da aplicação",
+        tags: ["api"], // ADD THIS TAG
+        validate: {
+          failAction: (req, res, error) => {
+            throw error;
+          },
+          query: Joi.object({
+            page: Joi.number(),
+            limit: Joi.number(),
+          }),
+        },
+        handler: coursesController.get,
+      },
+    },
+    // litar curso por id
+    {
+      method: "GET",
+      path: "/courses/{id}",
+      options: {
+        description: "Rota de cursos",
+        notes: "Buscar todos os cursos da aplicação",
+        tags: ["api"], // ADD THIS TAG
+        validate: {
+          failAction: (req, res, error) => {
+            throw error;
+          },
+          params: Joi.object({
+            id: Joi.string(),
+          }),
+        },
+        handler: coursesController.getById,
+      },
+    },
+    // criar curso
+    {
+      method: "POST",
+      path: "/courses",
+      options: {
+        description: "Rota criação de cursos",
+        notes: "Criar curso",
+        tags: ["api"], // ADD THIS TAG
+        validate: {
+          failAction: (req, res, error) => {
+            throw error;
+          },
+          payload: Joi.object({
+            idCategory: Joi.string().required(),
+            name: Joi.string().required(),
+            description: Joi.string().required(),
+          }),
+        },
+        handler: coursesController.create,
+      },
+    },
+    // alterar curso
+    {
+      method: "PUT",
+      path: "/courses/{id}",
+      options: {
+        description: "Rota alteracao de cursos",
+        notes: "Alterar curso",
+        tags: ["api"], // ADD THIS TAG
+        validate: {
+          failAction: (req, res, error) => {
+            throw error;
+          },
+          params: Joi.object({
+            id: Joi.string().required(),
+          }),
+          payload: Joi.object({
+            idCategory: {
+              _id: Joi.string(),
+            },
+            name: Joi.string(),
+            description: Joi.string(),
+          }),
+        },
+        handler: coursesController.put,
+      },
+    },
+    // remover curso
+    {
+      method: "DELETE",
+      path: "/courses/{id}",
+      options: {
+        description: "Rota remoção de curso",
+        notes: "Remover curso",
+        tags: ["api"], // ADD THIS TAG
+        validate: {
+          failAction: (req, res, error) => {
+            throw error;
+          },
+          params: Joi.object({
+            id: Joi.string().required(),
+          }),
+        },
+        handler: coursesController.delete,
+      },
+    },
+  ];
 
-    const coursesRoutes = [
-        // listas todos os cursos
-        {
-            method:'GET',
-            path: '/courses',
-            options: {
-                description: 'Rota de cursos',
-                notes: 'Buscar todos os cursos da aplicação',
-                tags: ['api'], // ADD THIS TAG
-                validate: {
-                    failAction: (req, res, error) => {
-                        throw error
-                    },
-                    query: Joi.object({
-                        page: Joi.number(),
-                        limit: Joi.number()
-                    })
-                },
-                handler: coursesController.get
-            }
-        },
-        // litar curso por id
-        {
-            method:'GET',
-            path: '/courses/{id}',
-            options: {
-                description: 'Rota de cursos',
-                notes: 'Buscar todos os cursos da aplicação',
-                tags: ['api'], // ADD THIS TAG
-                validate: {
-                    failAction: (req, res, error) => {
-                        throw error
-                    },
-                    params: Joi.object({
-                        id: Joi.string()
-                    })
-                },
-                handler: coursesController.getById
-            }
-        },
-        // criar curso
-        {
-            method:'POST',
-            path: '/courses',
-            options: {
-                description: 'Rota criação de cursos',
-                notes: 'Criar curso',
-                tags: ['api'], // ADD THIS TAG
-                validate: {
-                    failAction: (req, res, error) => {
-                        throw error
-                    },
-                    payload: Joi.object({
-                        idCategory: Joi.string().required(),
-                        name: Joi.string().required(),
-                        description: Joi.string().required()
-                    })
-                },
-                handler: coursesController.create
-            }
-        },
-        // alterar curso
-        {
-            method:'PUT',
-            path: '/courses/{id}',
-            options: {
-                description: 'Rota alteracao de cursos',
-                notes: 'Alterar curso',
-                tags: ['api'], // ADD THIS TAG
-                validate: {
-                    failAction: (req, res, error) => {
-                        throw error
-                    },
-                    params: Joi.object({
-                        id: Joi.string().required()
-                    }),
-                    payload: Joi.object({
-                        idCategory: Joi.string(),
-                        name: Joi.string(),
-                        description: Joi.string()
-                    })
-                },
-                handler: coursesController.put
-            }
-        },
-        // remover curso
-        {
-            method:'DELETE',
-            path: '/courses/{id}',
-            options: {
-                description: 'Rota remoção de curso',
-                notes: 'Remover curso',
-                tags: ['api'], // ADD THIS TAG
-                validate: {
-                    failAction: (req, res, error) => {
-                        throw error
-                    },
-                    params: Joi.object({
-                        id: Joi.string().required()
-                    })
-                },
-                handler: coursesController.delete
-            }
-        }
-    ]
-
-    server.route(coursesRoutes)
-}
+  server.route(coursesRoutes);
+};
